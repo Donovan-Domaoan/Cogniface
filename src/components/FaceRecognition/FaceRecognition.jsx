f263dbf00b7f1f3a4053794bbc904a3a363d6d93
Donovan D <lucasdomaoan@gmail.com>
2025-04-24 00:24:04 -0700

    added all complete working front end files

--- src/components/FaceRecognition/FaceRecognition.jsx
+++ src/components/FaceRecognition/FaceRecognition.jsx
@@ -1,9 +1,37 @@
 import React from "react";
+import './FaceRecognition.css';
 
-const FaceRecognition = ({ onButtonSubmit }) => {
+function FaceRecognition({ imageUrl, boxes }) {
+    const hideContent = !imageUrl || boxes.length === 0;
     return (
-        <div className='center'>
-            <img alt='' src={regions} />
+        <div className={`center ma ${hideContent ? 'hidden' : ''}`}>
+            <div className='absolute mt2'>
+                {imageUrl && (
+                    <img id='inputimage'
+                        src={imageUrl}
+                        alt='NO Detected Face'
+                        width='500px'
+                        height='auto' />
+                )}
+                {boxes.map((box, i) => (
+                    <div
+                        key={i}
+                        className='bounding-box'
+                        style={{
+                            top: box.topRow,
+                            right: box.rightCol,
+                            bottom: box.bottomRow,
+                            left: box.leftCol,
+                            position: 'absolute',
+                            boxShadow: '0 0 0 3px #149df2 inset',
+                            display: 'flex',
+                            flexWrap: 'wrap',
+                            justifyContent: 'center',
+                            cursor: 'pointer'
+                        }}
+                    ></div>
+                ))}
+            </div>
         </div>
     );
 }
